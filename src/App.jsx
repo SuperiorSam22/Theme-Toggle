9cb3302a604890601844bfdc37199d709abcb0bd
Sandeep Lal <sandeep221102@gmail.com>
2025-04-14 02:19:41 +0530

    background dark mode added

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [themeMode]);
   return (
     <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
-      <div className="flex flex-wrap min-h-screen items-center">
+      <div className="flex flex-wrap min-h-screen items-center dark:bg-gray-900">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeButton />
